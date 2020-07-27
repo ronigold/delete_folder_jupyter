031fabfdf913f8d1eb6074b456b4bf2deadfee68
roni <ronigoldsmid@gmail.com>
2020-07-27 13:13:27 +0300

    v0.0.3

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
 
+
 setup(
   name = 'delete_folder_jupyter',   
   packages = ['delete_folder_jupyter'],  
-  version = '0.0.2',      
+  version = '0.0.3',      
   license='MIT',      
   description = 'delete non-empty folder from jupyter and jupyter-lab',
   author = 'Roni Gold',                  
   author_email = 'ronigoldsmid@gmail.com', 
   url = 'https://github.com/ronigold',
-  download_url = 'https://github.com/ronigold/delete_folder_jupyter/archive/0.0.2.tar.gz',   
+  download_url = 'https://github.com/ronigold/delete_folder_jupyter/archive/0.0.3.tar.gz',   
   keywords = ['jupyter', 'jupyter-lab', 'delete non-empty folder'],  
   classifiers=[
     'Development Status :: 3 - Alpha',
