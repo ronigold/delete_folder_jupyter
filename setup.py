55e4d9e5dbdb2207ef3c6fc097719f133baa5a9e
roni <ronigoldsmid@gmail.com>
2020-07-27 13:11:13 +0300

    v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'delete_folder_jupyter',   
   packages = ['delete_folder_jupyter'],  
-  version = '0.0.1',      
+  version = '0.0.2',      
   license='MIT',      
   description = 'delete non-empty folder from jupyter and jupyter-lab',
   author = 'Roni Gold',                  
